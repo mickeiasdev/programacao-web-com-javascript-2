d32ed4640127522ee5fd4c1bef4cd905105d7f24
Mickeias Coelho <mickeiasdev@gmail.com>
2025-03-23 14:52:04 -0300

    Refactor validation functions and update script loading in DR4-TP3 project

--- DR4-TP3/DR4-TP3-1/script.js
+++ DR4-TP3/DR4-TP3-1/script.js
@@ -1,15 +1,25 @@
-const str = prompt("Digite uma palavra:");
+// Exercício 1 Sintaxe de Funções
+
+// Enunciado: Crie uma aplicação que implemente uma biblioteca de funções de validação de strings com 2 funções:
+// minLength e maxLength. Crie um script para testar cada uma das funções, solicitando ao usuário que digite valores para serem testados. 
+
+// Observações:
+
+// Utilize prompt e alert.
+// Implemente minLength usando a sintaxe de declaração de função.
+// Implemente maxLength usando a sintaxe de função de seta.
+
+const texto = prompt("Digite uma palavra:");
 const max = 10;
 const min = 5;
 
-if(maxLength(str, max)) {
-    alert(`A palavra é menor que ${max} caracteres`);
-} else {
-    alert(`A palavra é maior que ${max} caracteres`);
-}
-
-if(minLength(str, min)) {
-    alert(`A palavra é maior que ${min} caracteres`);
-} else {    
-    alert(`A palavra é menor que ${min} caracteres`);
+if(validarCancelar(texto)){
+    if(tamanhoMinimo(texto, min)) {
+        alert(`sua palavra esta dentro do limite minimo de ${min} caracteres`);
+        if(tamanhoMaximo(texto, max)) {
+            alert(`Sua palavra esta dentro do limite maximo de ${max} caracteres`);
+        } else {
+            alert(`A palavra é maior que ${max} caracteres`);
+        };
+    }
 }
