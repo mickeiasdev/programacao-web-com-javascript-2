02be465ef286a44f19e40ceb097acd6acd4f3c56
Mickeias Coelho <mickeiasdev@gmail.com>
2025-04-01 11:17:39 -0300

    Refactor HTML structure and JavaScript for exercises 4 and 5; update headings and add validation for array format

--- DR4-AT/biblioteca/biblioteca.js
+++ DR4-AT/biblioteca/biblioteca.js
@@ -1,3 +1,11 @@
 function validarAnoBissexto(ano) {
-    return (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0);
+  return (ano % 4 === 0 && ano % 100 !== 0) || ano % 400 === 0;
+}
+
+function verificarFormatoArray(item) {
+  if (typeof item !== "object" || !item.autor || !item.titulo || !item.isbn) {
+    throw new Error(
+      'Invalido! o item deve ter as propriedades: "Autor", "Titulo" e "Isbn".'
+    );
   }
+}
