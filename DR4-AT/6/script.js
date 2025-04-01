6e1fc97bb5e49548980f79791311b237d8d5d07b
Mickeias Coelho <mickeiasdev@gmail.com>
2025-04-01 11:27:12 -0300

    Update HTML titles and add content for exercises 1 to 6; enhance structure and styling

---
+++ DR4-AT/6/script.js
@@ -0,0 +1,16 @@
+// Enunciado: Crie uma página com um programa JavaScript que solicite um valor inteiro positivo e gere um array com a série Fibonacci dos números menores ou iguais ao número informado.
+
+// Observações:
+
+// Use input para obter o número.
+// Use “document.write” ou innerHTML para mostrar as mensagens no próprio HTML de saída: mostre o array gerado.
+// Não use recursividade nesta questão.
+// Dispare um erro caso o valor informado não seja válido. Use throw e o erro mais adequado ao tipo de problema encontrado.
+// Teste o seu programa com cenários diferentes.
+// Considerações:
+
+// Serão usados como teste os números 4181 e 6765.
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const numero = prompt("Digite um numero:")
+// });
